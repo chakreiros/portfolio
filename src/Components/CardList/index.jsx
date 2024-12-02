d60250c6a497e87ac024a3a27fb7c7d277f57f91
matheushnascimento <matheus.mhddn@gmail.com>
2024-12-02 16:01:08 -0300

    feat: add carousel to `CardList`

--- src/Components/CardList/index.jsx
+++ src/Components/CardList/index.jsx
@@ -1,12 +1,26 @@
 import { Card, CardHeader, CardTitle } from "@/Components/ui/card";
 
-import voOrlando from "@/assets/images/costumers/vo-orlando/vo-orlando(2).jpg";
-import belly from "@/assets/images/costumers/belly/Belly(3).jpg";
+import belly from "@/assets/images/costumers/belly/Belly.jpg";
+import belly1 from "@/assets/images/costumers/belly/Belly(1).jpg";
+import belly2 from "@/assets/images/costumers/belly/Belly(2).jpg";
+import belly3 from "@/assets/images/costumers/belly/Belly(3).jpg";
+import voOrlando1 from "@/assets/images/costumers/vo-orlando/vo-orlando(1).jpg";
+import voOrlando2 from "@/assets/images/costumers/vo-orlando/vo-orlando(2).jpg";
+import voOrlando3 from "@/assets/images/costumers/vo-orlando/vo-orlando(3).jpg";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 
 export default function CardList() {
   const cardData = [
-    { title: "Chácara Belly", description: "", image: belly },
-    { title: "Chácara do Vô Orlando", description: "", image: voOrlando },
+    {
+      title: "Chácara Belly",
+      description: "",
+      images: [belly, belly1, belly2, belly3],
+    },
+    {
+      title: "Chácara do Vô Orlando",
+      description: "",
+      images: [voOrlando1, voOrlando2, voOrlando3],
+    },
   ];
 
   return (
@@ -14,11 +28,27 @@ export default function CardList() {
       {cardData &&
         cardData.map((card, index) => (
           <Card
-            className=" max-w-[50vw] h-auto rounded-xl hover:cursor-pointer"
+            className="w-[50vw] h-auto rounded-xl hover:cursor-pointer"
             key={index}
           >
             <CardHeader>
-              <img className="rounded-xl" src={card.image} alt="" />
+              <Carousel className="w-fit h-full">
+                <CarouselContent>
+                  {Array.from({ length: card.images.length }).map(
+                    (_, index) => (
+                      <CarouselItem key={index}>
+                        <div>
+                          <img
+                            className="max-w-1/3 max-lg:max-w-full rounded-xl"
+                            src={card.images[index]}
+                            alt=""
+                          />
+                        </div>
+                      </CarouselItem>
+                    )
+                  )}
+                </CarouselContent>
+              </Carousel>
             </CardHeader>
 
             <CardTitle className="text-center text-green_1 m-6">
